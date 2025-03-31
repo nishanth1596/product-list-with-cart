9fe1c92b392cfb88c5544f8b4e463ff12f9ce606
Nishanth <nishanth1596@gmail.com>
2025-03-31 23:14:32 +0530

    Project complete

--- src/ui/Main/Main.tsx
+++ src/ui/Main/Main.tsx
@@ -3,12 +3,13 @@ import Cart from "../../features/cart/Cart";
 import Menu from "../../features/Menu/Menu";
 import OrderSuccessful from "../../features/Order/OrderSuccessful/OrderSuccessful";
 import { getIsOrderConfirmed } from "../../features/cart/cartSlice";
+import styles from "./Main.module.css";
 
 function Main() {
   const isOrdered = useSelector(getIsOrderConfirmed);
 
   return (
-    <main>
+    <main className={styles.main}>
       <Menu />
       {isOrdered ? <OrderSuccessful /> : <Cart />}
     </main>
